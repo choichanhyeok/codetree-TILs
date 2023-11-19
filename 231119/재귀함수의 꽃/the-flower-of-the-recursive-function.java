c1f1981a5f8ade4d4dea5e4cd3a560d01db7c1ea
chanhyeoKingOfDev <68278903+choichanhyeok@users.noreply.github.com>
2023-11-19 22:42:45 +0900

    Title: 재귀함수의 꽃, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 231119/재귀함수의 꽃/the-flower-of-the-recursive-function.java
+++ 231119/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -3,32 +3,21 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
-    private static void recursivePrintDesc(int x){
-        if (x == 1){
-            System.out.print(x + " ");
+    private static void recursivePrint(int n){
+        if (n == 0){
             return;
         }
 
-        System.out.print(x + " ");
-        recursivePrintDesc(x-1);
-    }
-
-    private static void recursivePrintAsc(int x, int n){
-        if (n == x){
-            System.out.print(x + " ");
-            return;
-        }
-
-        System.out.print(x +  " ");
-        recursivePrintAsc(x+1, n);
+        System.out.print(n + " ");
+        recursivePrint(n-1);
+        System.out.print(n + " ");
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        recursivePrintDesc(n);
-        recursivePrintAsc(1, n);
+        recursivePrint(n);
 
         br.close();
     }
