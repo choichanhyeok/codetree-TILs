2f21ea3978dd6302f9cc7df877d839421ec91a40
chanhyeoKingOfDev <68278903+choichanhyeok@users.noreply.github.com>
2023-11-19 20:18:37 +0900

    Title: 변수 출력하기 2, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231119/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char b = 'C';
+        String innerFix = "!.....!";
+
+        System.out.println(b + innerFix + a);
+    }
+}
