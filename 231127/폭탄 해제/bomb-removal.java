9c8f43e3e4cd09fed2423a9e440853e11969446e
chanhyeoKingOfDev <68278903+choichanhyeok@users.noreply.github.com>
2023-11-27 02:20:12 +0900

    Title: 폭탄 해제, Time: 120ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231127/폭탄 해제/bomb-removal.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String code = st.nextToken();
+        String targetLineColor = st.nextToken();
+        int targetSecond = Integer.parseInt(st.nextToken());
+
+        Bomb bomb = new Bomb(code, targetLineColor, targetSecond);
+        bw.write(bomb.about());
+
+        bw.flush();
+    }
+}
+
+
+class Bomb{
+    String code;
+    String targetLineColor;
+    int targetSecond;
+
+    public Bomb(String code, String targetLineColor, int targetSecond){
+        this.code = code;
+        this.targetLineColor = targetLineColor;
+        this.targetSecond = targetSecond;
+    }
+
+    public String about(){
+        return "code : " + this.code + "\ncolor : " + this.targetLineColor + "\nsecond : " + this.targetSecond;
+    }
+}
