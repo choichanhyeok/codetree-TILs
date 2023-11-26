71bd947b79ac39e272d765dc3eafafc7b282d7db
chanhyeoKingOfDev <68278903+choichanhyeok@users.noreply.github.com>
2023-11-27 02:28:01 +0900

    Title: 상품 코드, Time: 77ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231127/상품 코드/product-code.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        String productName = st.nextToken();
+        int productCode = Integer.parseInt(st.nextToken());
+
+        Product productA = new Product("leebros", 88);
+        Product productNew = new Product(productName, productCode);
+
+        bw.write(productA.about() + "\n");
+        bw.write(productNew.about());
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+class Product {
+    private final String productName;
+    private final int productCode;
+
+    public Product(String productName, int productCode){
+        this.productName = productName;
+        this.productCode = productCode;
+    }
+
+    public String about(){
+        return "product " + this.productCode + " is " + this.productName;
+    }
+}
