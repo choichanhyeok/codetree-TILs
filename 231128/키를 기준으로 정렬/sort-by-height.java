359c848932b0287ae427645646aaa391ca73c7e0
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-11-28 09:47:25 +0900

    Title: 키를 기준으로 정렬, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 231128/키를 기준으로 정렬/sort-by-height.java
+++ 231128/키를 기준으로 정렬/sort-by-height.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.Arrays;
 
 
-/**
-* TODO 2. Person 클래스는 Comparable 인터페이스를 상속 받아, compareTo를 오버라이드 해야함
-*/
 class Person implements Comparable<Person>{
-    String name;
-    int height, weight;
+    private final String name;
+    private final int height, weight;
 
     public Person(String name, int height, int weight){
         this.name = name;
@@ -16,34 +18,42 @@ class Person implements Comparable<Person>{
     }
 
     @Override
-    public int compareTo(Person person){
-        return this.height - person.height;
+    public int compareTo(Person other){
+        return this.height - other.height;
+    }
+
+    public String about(){
+        return this.name + " " + this.height + " " + this.weight;
     }
 }
 
 
-
 public class Main {
-    static final int MAXVALUE = 10;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // TODO 0. 입력: {입력 개수 n, n명의 이름-키-몸무게 정보}
-        int n = sc.nextInt();
-        Person[] persons = new Person[MAXVALUE];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // TODO 1. Person 객체 배열에 n 명의 개인정보 저장
+        int n = Integer.parseInt(br.readLine());
+        Person[] persons = new Person[n];
+
         for (int i = 0; i < n; i ++){
-            persons[i] = new Person(sc.next(), sc.nextInt(), sc.nextInt());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String name = st.nextToken();
+            int height = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            persons[i] = new Person(name, height, weight);
         }
 
-        // TODO 3. compareTo를 오버라이드한 Person 객체를 Arrays.sort로 정렬
-        Arrays.sort(persons, 0, n);
+        br.close();
+        Arrays.sort(persons);
 
-        // TODO 4. 오름차순 정렬된 Person형 객체 출력, (toString 오버라이드)
-        for (int i = 0; i < n; i ++){
-            System.out.print(persons[i].name + " ");
-            System.out.print(persons[i].height + " ");
-            System.out.println(persons[i].weight);
+        for (Person p: persons){
+            bw.write(p.about() + "\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 }
