6537aaf0b786bff2d66b813098a20b9ea4b654c8
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-11-29 00:51:54 +0900

    Title: 키를 기준으로 정렬, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231129/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Person[] persons = new Person[n];
+        for (int i = 0; i < n; i ++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int height = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            persons[i] = new Person(name, height, weight);
+        }
+        br.close();
+
+        Arrays.sort(persons);
+        for (Person p: persons){
+            bw.write(p.about() + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
+class Person implements Comparable<Person>{
+    private final String name;
+    private final int height;
+    private final int weight;
+
+    public Person(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Person other){
+        return this.height - other.height;
+    }
+
+    public String about(){
+        return this.name + " " + this.height + " " + this.weight;
+    }
+}
