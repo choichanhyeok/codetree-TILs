4630149075a1b1c29cd246845f7839673e8dfc14
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-01 11:56:24 +0900

    Title: 개인정보, Time: 251ms, Memory: 14MB, Status: Passed - CodeTree

--- 231201/개인정보/personal-info.java
+++ 231201/개인정보/personal-info.java
@@ -26,12 +26,7 @@ public class Main {
 
 
         // TODO 1. 이름 순으로 정렬
-        Arrays.sort(persons, new Comparator<Person>(){
-            @Override
-            public int compare(Person p, Person other){
-                return p.name.compareTo(other.name);
-            }
-        });
+        Arrays.sort(persons, (Person p, Person other) -> p.name.compareTo(other.name));
 
         bw.write("name\n");
         for (Person p: persons){
@@ -40,12 +35,7 @@ public class Main {
 
 
         // TODO 2. 키 순으로 정렬
-        Arrays.sort(persons, new Comparator<Person>(){
-            @Override
-            public int compare(Person p, Person other){
-                return other.height - p.height;
-            }
-        });
+        Arrays.sort(persons, (Person p, Person other) -> other.height - p.height);
 
         bw.write("\nheight\n");
         for (Person p: persons){
