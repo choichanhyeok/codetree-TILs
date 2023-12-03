71bfcd232b9c736bdc71cb0da342f54008de8190
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-04 07:51:02 +0900

    Title: 10진수로 변환하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 231204/10진수로 변환하기/convert-to-decimal.java
+++ 231204/10진수로 변환하기/convert-to-decimal.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] binary = new int[]{1, 1, 1, 0, 1};
-        int num = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i ++){
-            num = num * 2 + binary[i]; // 걍 공식임
-        }
+        String binary = sc.next();
+
+        int num = 0;
+        for(int i = 0; i < binary.length(); i++)
+            num = num * 2 + (binary.charAt(i) - '0');
         
         System.out.print(num);
     }
