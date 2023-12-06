e8fa323f41bdfa96b1718c83b361080e2bb11f55
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-06 22:28:50 +0900

    Title: 최대로 겹치는 구간, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 231206/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 231206/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,44 +1,45 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     private static final int OFFSET = 100;
-    private static final int MAX_N = OFFSET * 2 + 1;
-    private static final int[] line = new int[MAX_N];
-
+    private static final int MAX_R = OFFSET * 2 + 1;
+    private static final int[] line = new int[MAX_R];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-
+        
         for (int i = 0; i < n; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-
-            for (int j = start; j < end; j ++){
-                line[j + OFFSET] ++;
-            }
+            drawLine(start, end);
         }
-
         br.close();
-
-        bw.write(String.valueOf(getMaxOverlapping()));
+        bw.write(String.valueOf(getMaxOverlappingCount()));
         bw.flush();
         bw.close();
     }
 
-    public static int getMaxOverlapping(){
-        int maxValue = Integer.MIN_VALUE;
-
-        for (int i = 0; i < MAX_N; i ++){
-            maxValue = Math.max(maxValue, line[i]);
+    private static void drawLine(int start, int end){
+        for (int i = start; i < end; i ++){
+            line[OFFSET + i] ++;
         }
-        return maxValue;
+    }
+
+    private static int getMaxOverlappingCount(){
+        int MAX_VALUE = Integer.MIN_VALUE;
+
+        for (int i = 0; i < MAX_R; i++){
+            MAX_VALUE = Math.max(MAX_VALUE, line[i]);
+        }
+
+        return MAX_VALUE;
     }
 }
