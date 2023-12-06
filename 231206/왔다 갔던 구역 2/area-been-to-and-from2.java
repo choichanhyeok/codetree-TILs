90cbba940ae77eb93c4b2f2af7b5acd0ff964d51
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-06 22:55:28 +0900

    Title: 왔다 갔던 구역 2, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231206/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 231206/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -5,10 +5,11 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+
 public class Main {
     private static final int OFFSET = 100;
-    private static final int MAX_N = OFFSET * 2 + 1;
-    private static final int[] line = new int[MAX_N];
+    private static final int MAX_R = OFFSET * 2 + 1;
+    private static final int[] line = new int[MAX_R];
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,34 +21,33 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int distance = Integer.parseInt(st.nextToken());
             char direction = st.nextToken().charAt(0);
+
             move(distance, direction);
         }
-
         br.close();
-        bw.write(String.valueOf(getBetterThanTwoCount()));
+        bw.write(String.valueOf(getCountMovedTwice()-1));
         bw.flush();
         bw.close();
     }
 
-    private static int getBetterThanTwoCount(){
-        int count = 0;
-
-        for (int i = 0; i < MAX_N; i ++){
-            if (line[i] > 1){
-                count ++;
+    private static void move(int distance, char direction){
+        if (direction == 'L'){
+            while (--distance != -1){
+                line[OFFSET + distance] ++;
+            }
+        } else if (direction == 'R'){
+            while (--distance != -1){
+                line[OFFSET - distance] ++;
             }
         }
-        return count;
     }
 
-    private static void move(int distance, char direction){
-        if (direction == 'R'){
-            while (distance-- != 1)
-                line[OFFSET+distance] ++;
-            
-        } else{
-            while (distance-- != 1)
-                line[OFFSET-distance] ++;
+    private static int getCountMovedTwice(){
+        int answer = 0;
+        for (int i = 0; i < MAX_R; i ++){
+            if(line[i] > 1)
+                answer ++;
         }
+        return answer;
     }
 }
