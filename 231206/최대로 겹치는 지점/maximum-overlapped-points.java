257472caddf7686f15cb85b36e6405e70cefab3f
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-06 12:22:35 +0900

    Title: 최대로 겹치는 지점, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 231206/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 231206/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -1,43 +1,44 @@
-import java.util.Scanner;
-
-// 문제 분석
-// 1. 직선상에 선분이 놓여 있음 _______________xxxx____ : n은 무조건 양수
-// 2. 가장 많이 겹치는 곳에서 몇개의 선분이 겹치는지 구하시오.
-// 3. 단, 끝점 닿는 경우도 겹치는 것으로 보기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    // 필요 변수
-    private static int n;
-    private static final int MAX_R = 100 + 1;
-    private static final int[] checked = new int[MAX_R];
+    private static final int OFFSET = 100;
+    private static final int MAX_N = OFFSET * 2 + 1;
+    private static final int[] line = new int[MAX_N];
 
-    private static void drawLine(int a, int b){
-        for(int i = a; i <= b; i++){
-            checked[i] += 1;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i ++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            for (int j = start; j <= end; j ++){
+                line[j] ++;
+            }
         }
+        br.close();
+        bw.write(String.valueOf(getMaxOverlappingPartCount()));
+        bw.flush();
+        bw.close();
     }
 
-    private static int getMaxValue(){
-        int maxValue = 0;
-        for (int i = 0; i < MAX_R; i ++){
-            maxValue = Math.max(maxValue, checked[i]);
+    private static int getMaxOverlappingPartCount(){
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < MAX_N; i ++){
+            maxValue = Math.max(maxValue, line[i]);
         }
 
         return maxValue;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // TODO 1. 선분의 개수 n입력
-        n = sc.nextInt();
-
-        // TODO 2. n개의 선분을 입력받기
-        for (int i = 0; i < n; i ++){
-            // TODO 3. 입력받은 각각의 선분을 checked에 기록 (+=1 방식으로)
-            drawLine(sc.nextInt(), sc.nextInt());
-        }
-        // TODO 4. 가장 많이 겹치는 선분의 크기 구하기
-        System.out.println(getMaxValue());
     }
 }
