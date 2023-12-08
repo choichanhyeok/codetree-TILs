7212bc0c7c1a53c82c1b6117d7fd8802ad12e383
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-08 13:47:02 +0900

    Title: 사각형의 총 넓이 2, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

--- 231208/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 231208/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -18,10 +18,10 @@ public class Main {
 
         for (int i = 0; i < n; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int y1 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int x2 = Integer.parseInt(st.nextToken()) + OFFSET;
+            int y2 = Integer.parseInt(st.nextToken()) + OFFSET;
 
             for (int j = x1; j < x2; j ++){
                 for (int k = y1; k < y2; k ++){
