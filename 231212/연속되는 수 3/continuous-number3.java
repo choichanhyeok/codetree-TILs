b85dee6c1faf18081235030ba4c3d7c86ca12569
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-12 12:18:36 +0900

    Title: 연속되는 수 3, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 231212/연속되는 수 3/continuous-number3.java
+++ 231212/연속되는 수 3/continuous-number3.java
@@ -1,46 +1,30 @@
-import java.util.Scanner;
-
-// 문제 분석
-// 1. 0이 아닌 N개의 숫자가 주어짐
-// 2. 부호가 동일한 숫자로 이루어진 연속 부분 수열 탐색
-// 3. 가장 큰 연속 부분 수열의 길이 출력
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    // 필요 변수
-    private static final int MAX_R = 1000 + 1;
-    private static final int[] numbArray = new int[MAX_R];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static int getMaxSequencLength(int n){
-        int seqCnt = 0;
-        int maxValue = 0;
-        boolean isPlus;
-
-        for (int i = 0; i < n; i++){
-            isPlus = (numbArray[i] > 0);
-            seqCnt += 1;
-            if (i == 0 || (0 < numbArray[i-1]) != isPlus){
-                maxValue = Math.max(maxValue, seqCnt);
-                seqCnt = (i == 0)? 1: 0;
-            }
-        }
-
-        maxValue = Math.max(maxValue, seqCnt);
-        return maxValue;
-
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // TODO 1. 숫자들의 개수 n 값 입력
-        int n = sc.nextInt();
-
-        // TODO 2. n개의 숫자를 배열에 세팅
+        int n = Integer.parseInt(br.readLine());
+        int[] numbers = new int[n];
+        int maxCount = Integer.MIN_VALUE;
+        int count = 0;
         for (int i = 0; i < n; i ++){
-            numbArray[i] = sc.nextInt();
+            numbers[i] = Integer.parseInt(br.readLine());
+            if (i == 0 || (numbers[i] * numbers[i-1]) < 0){
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            }
+            count ++;
         }
+        maxCount = Math.max(maxCount, count);
 
-        // TODO 3. 음양을 구분해 부분 수열 탐색, 각각의 길이를 확인해 최대 값 추출
-        System.out.println(getMaxSequencLength(n));
+        bw.write(String.valueOf(maxCount));
+        bw.flush();
+        bw.close();
     }
 }
