062598db09ed35f9cdc026b76c2e56e2425ab0fe
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-13 00:01:56 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231213/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        int MAX_COUNT = Integer.MIN_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            int nowNumb = Integer.parseInt(st.nextToken());
+            if (i == 0 || nowNumb <= t){
+                MAX_COUNT = Math.max(MAX_COUNT, nowNumb);
+                count = 0;
+            }
+            count ++;
+        }
+
+        br.close();
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+
+    }
+}
