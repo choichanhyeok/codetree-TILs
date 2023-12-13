0e6a98cd1c3db1e2fba6f1edfe2063e0b677766c
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-13 23:54:05 +0900

    Title: 만나는 그 순간, Time: 93ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 231213/만나는 그 순간/the-moment-we-meet.java
+++ 231213/만나는 그 순간/the-moment-we-meet.java
@@ -1,70 +1,68 @@
-import java.util.Scanner;
-
-// 문제 분석
-// 1. A와 B는 동일한 시작점에서 출발
-// 2. 각각 N, M번에 걸쳐 움직인다.
-// 3. 시간 기준으로 각 배열에 위치 값을 저장한 뒤
-// 4. 같은 시간에 같은 위치 (같은 index에 같은 value)를 갖는 곳을 찾는다. (없을시 -1)
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    // 필요 변수
-    private static final int OFFSET = 1000000;
-    private static final int MAX_R = OFFSET * 2 + 1;
-    private static final int[] positionA = new int[MAX_R];
-    private static final int[] positionB = new int[MAX_R];
-    private static final boolean isA = true;
-    private static final boolean isNotA = false;
-    private static int time = 1;
-    private static int endPoint = 0;
+    private static final int MAX_R = 1000000;
+    private static final int[] lineA = new int[MAX_R];
+    private static final int[] lineB = new int[MAX_R];
 
-    private static void setPositionByTimeAndDirection(char direction, int distance, boolean isA){        
-        while (distance-- > 0){
-            if (isA){
-                positionA[time] = positionA[time-1] + ((direction == 'R')? 1: -1); 
-                time ++;
-            }else{
-                positionB[time] = positionB[time-1] + ((direction == 'R')? 1: -1);
-                time ++;
-            }
-        }
-        endPoint = time;
-    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static int getFirstMeet(){
-        int answer = -1;
-        for (int i = 1; i < time ; i++){
-            if (positionA[i] == positionB[i]){
-                answer = i;
-                break;
-            }
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // TODO 1. N과 M을 입력받는다.
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        
-        // TODO 2. direction과 distance를 입력받아 시간 배열 positionA, positionB에 현재 위치들을 저장
+        // TODO 1. 각 A, B가 매 초마다 어느 위치에 있는지 기록
+        int currentA = 0;
+        int aSize = 0;
         for (int i = 0; i < n; i ++){
-            char direction = sc.next().charAt(0);
-            int distance = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            char direction = st.nextToken().charAt(0);
+            int distance = Integer.parseInt(st.nextToken());
 
-            setPositionByTimeAndDirection(direction, distance, isA);
+            while (distance-- != 0){    
+                currentA += (direction == 'R')? 1: -1;
+                lineA[i] = currentA;
+                aSize ++;
+            }
         }
 
-        time = 1; // time 1로 초기화
+        int currentB = 0;
+        int bSize = 0;
         for (int i = 0; i < m; i ++){
-            char direction = sc.next().charAt(0);
-            int distance = sc.nextInt();
-            
-            setPositionByTimeAndDirection(direction, distance, isNotA);
-        }
+            st = new StringTokenizer(br.readLine());
+            char direction = st.nextToken().charAt(0);
+            int distance = Integer.parseInt(st.nextToken());
 
-        // TODO 3. 저장된 위치들 중 같은 시간에 같은 위치를 갖는 곳의 첫 위치를 출력한다. (없으면 -1)
-        System.out.println(getFirstMeet());
+            while (distance-- != 0){
+                currentB += (direction == 'R')? 1: -1;
+                lineB[i] = currentB;
+                bSize ++;
+            }
+        }
+        br.close();
+
+        // TODO 2. A와 B가 만나는 시간 추력
+        int meetCount = 0;
+        boolean isMeet = false;
+        for (int i = 0; i < Math.min(aSize, bSize); i ++){;
+            meetCount ++;
+            if(lineA[i] == lineB[i]){
+                bw.write(String.valueOf(meetCount));
+                isMeet = true;
+            }
+        }
+        System.out.println(meetCount);
+        if (!isMeet)
+            bw.write("-1");
+
+        bw.flush();
+        bw.close();
     }
 }
