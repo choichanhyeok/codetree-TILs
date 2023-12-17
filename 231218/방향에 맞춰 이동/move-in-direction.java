c644f6aab83e34c4fd0907497bde048a9cdb2f0f
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-18 07:40:31 +0900

    Title: 방향에 맞춰 이동, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 231218/방향에 맞춰 이동/move-in-direction.java
+++ 231218/방향에 맞춰 이동/move-in-direction.java
@@ -1,50 +1,38 @@
-import java.util.Scanner;
-
-// 문제 분석
-// 1. (0, 0)에서 N번 움직이려고 한다
-// 2. N번에 걸쳐 움직이는 방향과 거리가 주어졌고
-// 3. 최종 위치를 출력하는 프로그램을 작성하라
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static final int OFFEST = 1000;
-    private static final int MAX_R = OFFEST * 2 + 1;
-    private static final int[][] matrix = new int[MAX_R][MAX_R];
-    private static int[] dx = {0, 1, 0, -1};
-    private static int[] dy = {1, 0, -1, 0};
-    private static int nx, ny;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static final void move(char direction, int distance){
-        if (direction == 'N'){
-            nx += dx[0] * distance;
-            ny += dy[0] * distance;
-        }else if (direction == 'E'){
-            nx += dx[1] * distance;
-            ny += dy[1] * distance;
-        }else if (direction == 'S'){
-            nx += dx[2] * distance;
-            ny += dy[2] * distance;
-        }else{
-            nx += dx[3] * distance;
-            ny += dy[3] * distance;
-        }
+        int n = Integer.parseInt(br.readLine());
 
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        // TODO 1. n번에 걸쳐 움직이는 방향과 거리
+        int currentX = 0;
+        int currentY = 0;
         for (int i = 0; i < n; i ++){
-            char direction = sc.next().charAt(0);
-            int distance = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char direction = st.nextToken().charAt(0);
+            int distance = Integer.parseInt(st.nextToken());
 
-            // TODO 2. 명령에 따라 nx, ny 위치 적용해주기
-            move(direction, distance);
+            if (direction == 'W')
+                currentX -= distance;
+            else if (direction == 'E')
+                currentX += distance;
+            else if (direction == 'N')
+                currentY += distance;
+            else if (direction == 'S')
+                currentY -= distance;
         }
+        br.close();
 
-        // TODO 3. 현재 nx, ny의 값 출력
-        System.out.println(nx + " " + ny);
+        bw.write(currentX + " " + currentY);
+        bw.flush();
+        bw.close();
 
     }
 }
