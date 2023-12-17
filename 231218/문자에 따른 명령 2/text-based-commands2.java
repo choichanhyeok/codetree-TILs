545b947f348a2b486ec1b51131efa30ae7438356
chanhyeoKingOfDev <cksgurwkd12@naver.com>
2023-12-18 08:43:28 +0900

    Title: 문자에 따른 명령 2, Time: 100ms, Memory: 11MB, Status: Passed - CodeTree

--- 231218/문자에 따른 명령 2/text-based-commands2.java
+++ 231218/문자에 따른 명령 2/text-based-commands2.java
@@ -1,33 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    private static final int[] dx = new int[]{0, 1, 0, -1};
-    private static final int[] dy = new int[]{1, 0, -1, 0};
-    private static int nx, ny;
+    private static final int[] dys = new int[]{1, 0, -1, 0};
+    private static final int[] dxs = new int[]{0, 1, 0, -1};
+    private static int x, y;
 
-    private static void moveBasedOnCommand(String commands){
-        int dirIdx = 0;
-        for (char command: commands.toCharArray()){
-            if (command == 'L')
-                dirIdx = (dirIdx - 1 + 4) % 4;
-            else if (command == 'R')
-                dirIdx = (dirIdx + 1) % 4;
-            else if (command == 'F'){
-                nx += dx[dirIdx];
-                ny += dy[dirIdx];
-            }
-        }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String commands = br.readLine();
+        br.close();
+
+        move(commands);
+        bw.write(x + " " + y);
+        bw.flush();
+        bw.close();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static void move(String commands){
+        int currentDirection = 0;
 
-        // TODO 1. 명령의 집합 입력받기
-        String commands = sc.next();
-
-        // TODO 2. 명령에 따라 이동
-        moveBasedOnCommand(commands);
-
-        System.out.println(nx + " " + ny);
+        for (char command: commands.toCharArray()){
+            if (command == 'R')
+                currentDirection = (currentDirection + 1) % 4;
+            else if (command == 'L')
+                currentDirection = (currentDirection - 1 + 4) % 4;
+            else if (command == 'F'){
+                x += dxs[currentDirection];
+                y += dys[currentDirection];
+            }
+        }
     }
 }
